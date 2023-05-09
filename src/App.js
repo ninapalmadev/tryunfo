8a791aff9a9216e61c8a08215719e3f07cbb1ab0
Nina Palma <ninapalmadev@gmail.com>
2023-05-09 19:10:15 -0300

    requisito 5

--- src/App.js
+++ src/App.js
@@ -14,16 +14,65 @@ class App extends React.Component {
     cardRare: 'normal',
     cardTrunfo: true,
     // hasTrunfo: false,
-    // isSaveButtonDisabled: true,
-    // onInputChange: '',
-    // onSaveButtonClick: '',
+    onInputChange: '',
+    isSaveButtonDisabled: true,
+    onSaveButtonClick: '',
+    savedCards: [],
   };
 
   onInputChange = (event) => {
-    const { name, value } = event.target;
+    const { name, type, checked, value } = event.target;
+    const valueField = type === 'checkbox' ? checked : value;
+
     this.setState({
-      [name]: value,
-    });
+      [name]: valueField,
+    }, () => this.validateSaveButton());
+  };
+
+  onSaveButtonClick = (event) => {
+    event.preventDefault();
+    const cardInfo = { ...this.state };
+    this.setState((prev) => ({
+      savedCards: [...prev.savedCards, cardInfo],
+    }));
+  };
+
+  validateSaveButton = () => {
+    const {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+    } = this.state;
+
+    const min = 0;
+    const max = 90;
+    const total = 210;
+
+    const attack = Number(cardAttr1);
+    const attack2 = Number(cardAttr2);
+    const attack3 = Number(cardAttr3);
+
+    if (cardName === ''
+    || cardDescription === ''
+    || cardImage === ''
+    || attack > max
+    || attack < min
+    || attack2 > max
+    || attack2 < min
+    || attack3 > max
+    || attack3 < min
+    || (attack + attack2 + attack3) > total) {
+      this.setState(() => ({
+        isSaveButtonDisabled: true,
+      }));
+    } else {
+      this.setState(() => ({
+        isSaveButtonDisabled: false,
+      }));
+    }
   };
 
   render() {
@@ -33,6 +82,7 @@ class App extends React.Component {
           <h1>Tryunfo</h1>
           <Form
             onInputChange={ this.onInputChange }
+            onSaveButtonClick={ this.onSaveButtonClick }
             { ...this.state }
           />
         </div>
